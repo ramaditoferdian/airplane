9f22e0e6d8c1b4b00da5678548d008113d09e454
Ramadito Ferdian Assa <ramaditoferdian@gmail.com>
2022-02-06 20:52:23 +0700

    add choose seat page

---
+++ lib/ui/pages/choose_seat_page.dart
@@ -0,0 +1,38 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:airplane/shared/theme.dart';
+import 'package:flutter/material.dart';
+
+class ChooseSeatPage extends StatelessWidget {
+  const ChooseSeatPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Widget title() {
+      return Container(
+        margin: EdgeInsets.only(
+          top: 30,
+        ),
+        child: Text(
+          'Select Your\nFavorite Seat',
+          style: blackTextStyle.copyWith(
+            fontSize: 24,
+            fontWeight: semiBold,
+          ),
+        ),
+      );
+    }
+
+    return Scaffold(
+      backgroundColor: kBackgroundColor,
+      body: ListView(
+        padding: EdgeInsets.symmetric(
+          horizontal: 24,
+        ),
+        children: [
+          title(),
+        ],
+      ),
+    );
+  }
+}
--- lib/ui/pages/detail_page.dart
+++ lib/ui/pages/detail_page.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:airplane/shared/theme.dart';
+import 'package:airplane/ui/pages/choose_seat_page.dart';
 import 'package:airplane/ui/widgets/custom_button.dart';
 import 'package:airplane/ui/widgets/interest_item.dart';
 import 'package:airplane/ui/widgets/photo_item.dart';
@@ -254,7 +255,14 @@ class DetailPage extends StatelessWidget {
                   // ! Note : Book Button
                   CustomButton(
                     title: 'Book Now',
-                    onPressed: () {},
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => ChooseSeatPage(),
+                        ),
+                      );
+                    },
                     width: 170,
                   )
                 ],
